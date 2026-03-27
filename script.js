0133d86230cc2cf01719d9ba326790a63672819d
thakurivinay94-ship-it <thakurivinay94@gmail.com>
2026-03-28 00:33:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let message = "I Love You So Much ❤️ You Are My Forever 💕";
+let i = 0;
+
+function typeEffect() {
+  if (i < message.length) {
+    document.getElementById("text").innerHTML += message.charAt(i);
+    i++;
+    setTimeout(typeEffect, 50);
+  }
+}
+
+function openLetter() {
+  document.getElementById("letter").style.display = "block";
+
+  typeEffect();
+
+  let music = document.getElementById("music");
+  music.play();
+
+  startSlideshow();
+}
+
+/* Slideshow */
+let images = ["photo1.jpg", "photo2.jpg", "photo3.jpg"];
+let index = 0;
+
+function startSlideshow() {
+  setInterval(() => {
+    index = (index + 1) % images.length;
+    document.getElementById("slide").src = images[index];
+  }, 2000);
+}
